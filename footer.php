4fe64e4ee7408d41af5a4e2e295be121547751ce
Alex Green <alex@devfarms.com>
2025-10-24 13:53:24 -0700

    Footer changes. Fonts applied. Header changes.

--- footer.php
+++ footer.php
@@ -1,12 +1,13 @@
 <footer class="site-footer">
         <div class="container">
             <div class="footer-content">
-                
-                <div class="footer-info">
-                    <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. All rights reserved.</p>
-                </div>
-                
-                <?php if (has_nav_menu('footer')) : ?>
+                <div class="footer-columns">
+                    <div class="footer-column-1">
+                        <img src="wp-content/themes/skate-spots-theme/assets/images/brnb_white_wheel.png" class="white-wheel">
+                    </div>
+                    <div class="footer-column-2">
+                        <?php if (current_user_can('manage_options') && has_nav_menu('footer')) : ?>
+                    <?php if (has_nav_menu('footer')) : ?>
                     <nav class="footer-navigation">
                         <?php
                         wp_nav_menu(array(
@@ -17,8 +18,15 @@
                         ?>
                     </nav>
                 <?php endif; ?>
+                <?php endif; ?>
+                    </div>
+                    <div class="footer-column-3">Column 3</div>
+                
                 
             </div>
+        </div>
+        <div class="footer-info">
+            <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. All rights reserved.</p>
         </div>
     </footer>
 
--- front-page.php
+++ front-page.php
@@ -10,7 +10,7 @@ get_header(); ?>
     <?php while ( have_posts() ) : the_post(); ?>
         <?php if ( get_the_title() ) : ?>
             <header class="entry-header">
-                <h1 class="entry-title"><?php the_title(); ?></h1>
+                <h1 class="entry-title front-page-title"><?php the_title(); ?></h1>
             </header>
         <?php endif; ?>
     <?php endwhile; ?>
@@ -31,7 +31,7 @@ get_header(); ?>
     </div> 
     <div <?php post_class('maps-content'); ?>>
         
-        <div class="entry-content">
+        <div class="entry-content front-page-title">
     <?php echo do_shortcode('[skate_scenes_list]'); ?>
         </div>
         
